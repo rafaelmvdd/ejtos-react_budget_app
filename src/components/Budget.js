1047655460547f18207c8295edc3daae82a27460
rafaelmvdd <rafaelmvdd@gmail.com>
2024-01-02 22:26:49 -0500

    Final Project changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,21 +2,63 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-  const { budget } = useContext(AppContext);
+  const { budget, expenses, dispatch, currency } = useContext(AppContext);
   const [newBudget, setNewBudget] = useState(budget);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
   const handleBudgetChange = (event) => {
+    if (event.target.value > 20000) {
+      alert('The budget value cannot exceed £20000');
+    }
+    if(event.target.value < totalExpenses) {
+      alert('You cannot reduce the budget value lower than the spending')
+    }
     setNewBudget(event.target.value);
   };
+  const handleCurrencyChange = (event) => {
+    dispatch({
+      type: 'CHG_CURRENCY',
+      payload: event.target.value,
+    });
+    
+  }
   return (
-    <div className="alert alert-secondary">
-      <span>Budget: £{budget}</span>
+    <div>
+      <div className="alert alert-secondary">
+      <span>Budget: {currency}</span>
       <input
         type="number"
         step="10"
         value={newBudget}
-        onChange={handleBudgetChange}
+        onChange={(event) => handleBudgetChange(event)}
       ></input>
     </div>
+    <div>
+      <select
+            className="custom-select"
+            id="inputGroupSelect01"
+            onChange={(event) => handleCurrencyChange(event)}
+            value={currency}
+          >
+            <option defaultValue>Choose...</option>
+            <option value="$" name="dollar">
+              {' '}
+              $ Dollar
+            </option>
+            <option value="£" name="pound">
+              £ Pound
+            </option>
+            <option value="€" name="euro">
+              € Euro
+            </option>
+            <option value="₹" name="ruppee">
+              ₹ Ruppee
+            </option>
+          </select>
+    </div>
+    </div>
+    
   );
 };
 export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const { dispatch } = useContext(AppContext);
+  const { dispatch, currency } = useContext(AppContext);
 
   const handleDeleteExpense = () => {
     dispatch({
@@ -24,12 +25,26 @@ const ExpenseItem = (props) => {
     });
   };
 
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: -10,
+    };
+
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    });
+  };
   return (
     <tr>
       <td>{props.name}</td>
-      <td>£{props.cost}</td>
+      <td>{currency}{props.cost}</td>
       <td>
-        <button onClick={(event) => increaseAllocation(props.name)}>+</button>
+        <button className="add_button" onClick={(event) => increaseAllocation(props.name)}>+</button>
+      </td>
+      <td>
+        <button className="minus_button" onClick={(event) => decreaseAllocation(props.name)}>-</button>
       </td>
       <td>
         <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
